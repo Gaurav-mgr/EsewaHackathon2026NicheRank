be0384a19eb566bf46dfe62fc4ad317de7dcd774
Gaurav-mgr <gauravpulami5@gmail.com>
2026-05-29 00:24:57 +0545

    updated UI of homepage

--- NicheRankHackathon/src/components/HomepageComp/CarousalList/Service.js
+++ NicheRankHackathon/src/components/HomepageComp/CarousalList/Service.js
@@ -1,31 +1,31 @@
 export default [
   {qr:{
     overlay: "Request QR",
-    image: "https://static.vecteezy.com/system/resources/thumbnails/036/230/794/small_2x/ai-generated-vibrant-raindrop-sphere-reflects-nature-beauty-in-abstract-free-photo.jpg",
+    image: "src/assets/Business Qr Request.webp",
     title: "Business QR Request",
   },
 
   espeaker:{
     overlay: "eSpeaker Request",
-    image: "https://static.vecteezy.com/system/resources/thumbnails/036/230/794/small_2x/ai-generated-vibrant-raindrop-sphere-reflects-nature-beauty-in-abstract-free-photo.jpg",
+    image: "src/assets/eSpeaker Request.webp",
     title: "eSpeaker Request",
   },
 
   ielts:{
     overlay: "IELTS",
-    image: "https://static.vecteezy.com/system/resources/thumbnails/036/230/794/small_2x/ai-generated-vibrant-raindrop-sphere-reflects-nature-beauty-in-abstract-free-photo.jpg",
+    image: "src/assets/IELTS Registration.webp",
     title: "IELTS Registration",
   },
 
   alfa:{
     overlay: "Up to 50% Cashback",
-    image: "https://static.vecteezy.com/system/resources/thumbnails/036/230/794/small_2x/ai-generated-vibrant-raindrop-sphere-reflects-nature-beauty-in-abstract-free-photo.jpg",
+    image: "src/assets/NAME heath.webp",
     title: "Alfa Health Care & Diagnostic Center",
   },
 
   meroshare:{
     overlay: "Meroshare",
-    image: "https://static.vecteezy.com/system/resources/thumbnails/036/230/794/small_2x/ai-generated-vibrant-raindrop-sphere-reflects-nature-beauty-in-abstract-free-photo.jpg",
+    image: "src/assets/Alfa heath care & Diagnostic center.webp",
     title: "Naasa Securities Company Limited",
   }}
 ]
--- NicheRankHackathon/src/components/HomepageComp/CarousalList/Spotlight.js
+++ NicheRankHackathon/src/components/HomepageComp/CarousalList/Spotlight.js
@@ -1,31 +1,31 @@
 export default [
   {cn:{
     overlay: "10% Discount",
-    image: "https://static.vecteezy.com/system/resources/thumbnails/036/230/794/small_2x/ai-generated-vibrant-raindrop-sphere-reflects-nature-beauty-in-abstract-free-photo.jpg",
+    image: "src/assets/colorNepal.webp",
     title: "Colour Nepal",
   },
 
   es:{
     overlay: "10% Discount",
-    image: "https://static.vecteezy.com/system/resources/thumbnails/036/230/794/small_2x/ai-generated-vibrant-raindrop-sphere-reflects-nature-beauty-in-abstract-free-photo.jpg",
+    image: "src/assets/EglinesStore.webp",
     title: "Eglines Store",
   },
 
   tos:{
     overlay: "10% Discount",
-    image: "https://static.vecteezy.com/system/resources/thumbnails/036/230/794/small_2x/ai-generated-vibrant-raindrop-sphere-reflects-nature-beauty-in-abstract-free-photo.jpg",
+    image: "src/assets/tsArmoire.jpg",
     title: "Tsarmoire Online Store Pvt. Ltd.",
   },
 
   al:{
     overlay: "10% Discount",
-    image: "https://static.vecteezy.com/system/resources/thumbnails/036/230/794/small_2x/ai-generated-vibrant-raindrop-sphere-reflects-nature-beauty-in-abstract-free-photo.jpg",
+    image: "src/assets/aasaLuxe.webp",
     title: "Aasa Luxe",
   },
 
   oppl:{
     overlay: "3% Discount",
-    image: "https://static.vecteezy.com/system/resources/thumbnails/036/230/794/small_2x/ai-generated-vibrant-raindrop-sphere-reflects-nature-beauty-in-abstract-free-photo.jpg",
+    image: "src/assets/Oppl.jpg",
     title: "Oometo Partners Private Limited",
   }}
 ]
